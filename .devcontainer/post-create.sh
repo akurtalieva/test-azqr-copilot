14475e39642cb85329d551d5507f62505f350467
alejandrolmeida <alejandro.almeida.garcia@gmail.com>
2024-04-16 22:32:26 +0200

    Add devcontainers ecosystem

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,15 @@
+## CaskaydiaCove Nerd Font
+# Uncomment the below to install the CaskaydiaCove Nerd Font
+mkdir $HOME/.local
+mkdir $HOME/.local/share
+mkdir $HOME/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaCode.zip
+unzip CascadiaCode.zip -d $HOME/.local/share/fonts
+rm CascadiaCode.zip
+
+## Install Oh-my-posh 
+# https://ohmyposh.dev/docs/installation/linux
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
+echo "alias ll='ls -alF'" >> ~/.bashrc
+echo 'eval "$(oh-my-posh init bash --config /usr/local/share/omp-templates/almeida.omp.json)" ' >> ~/.bashrc
